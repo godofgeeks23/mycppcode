2a215b2b7d6fd09781ac4cc752ccca57061bd245
Aviral Srivastava <aviralji4@gmail.com>
2020-12-07 15:59:59 +0530

    "updated once again..."

--- cpp_projs/quicktest.cpp
+++ cpp_projs/quicktest.cpp
@@ -1,14 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
-// int main()
-// {
-//     int d1, v1, d2, v2, p, d;
-//     d1 = d2 = 1;
-//     cin>>d1>>v1>>d2>>v2>>p;
-//     float temp = (float(p) + (float(v1)*float(d1)) + (float(v2)*float(d2)) - (float(v1) + float(v2)))/(float(v1) + float(v2));
-//     cout<<ceil(temp)<<endl;
-//     return 0;
-// }
-
-
-
+int main()
+{
+    
+    return 0;
+}
---
+++ cpp_projs/recap8.cpp
@@ -0,0 +1,20 @@
+//recap8.cpp
+
+/*
+This is a single cpp file which aims to quickly revise all the previous studied cpp topics.
+This contains some theory part, syntax, as well as some quick points which need to be remembered.
+Have a quick glance at this file periodically or once in a while! 
+*/
+
+/*
+The topics covered in this file are - 
+    inheritance
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+    return 0;
+}

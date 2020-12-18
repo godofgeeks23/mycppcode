48828b908b28239cab65c8def76839b9bd5ddbbc
Aviral Srivastava <aviralji4@gmail.com>
2020-12-18 09:31:35 +0530

    "modified the project structure..."

--- cpp_projs/cp/kiet.c
+++ cpp_projs/cp/kiet.c
@@ -8,16 +8,10 @@ int main()
 {
 	int a = 10,b = 20;
 	float avg;
-
 	clrscr();
-
 	scanf("%d%d",&a, &b);
 	avg = (a+b)/2.0;
-
 	printf("Avg is: %d ... Hope you like it!", avg);
 	//comiler reads printf from right to left 
-
-	
-
 	getch();
 }

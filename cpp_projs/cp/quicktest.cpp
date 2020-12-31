76044eef17dea4dfd0fc0a8a3f7b8c995477d340
Aviral Srivastava <aviralji4@gmail.com>
2020-12-31 12:36:48 +0530

    "updated the code base..."

--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -1,12 +1,83 @@
-#include<bits/stdc++.h>
+#include<bits/stdc++.h>                 // CODECHEF - BYTR20B/RICY
 
 using namespace std;
-
+int rangemin(int* arr, int a, int b)
+{
+    int min = arr[a-1];
+    for(int i=(a-1);i<=(b-1);i++)
+    {
+        if(arr[i]<min)
+            min = arr[i];
+    }
+    return min;
+}   
 int main()
 {
-    
+    int t,m,n;
+    int a[100001], b[100001];
+    cin>>t;
+    while(t--)
+    {
+        scanf("%d%d", &n, &m);
+        for(int i=0;i<n;i++)
+            scanf("%d", &a[i]);
+        for(int i=0;i<m;i++)
+            scanf("%d", &b[i]);
+        int sum = 0;
+        for(int i=0;i<m;i++)
+        {
+            for(int j=i;j<m;j++)
+            {
+                sum+=rangemin(a, b[i], b[j]);
+            }
+        }
+        printf("%d\n", sum);
+    }
     return 0;
 }
+
+// #include<iostream>                                   //  CODECHEF - AGCY
+// using namespace std;
+// int main()
+// {
+//     int t,n,q,l,r;
+//     int cities[100001] = {};
+//     scanf("%d", &t);
+//     while(t--)
+//     {
+//         scanf("%d%d", &n, &q);
+//         while(q--)
+//         {
+//             scanf("%d%d", &l, &r);
+//             for(int i=l;i<=r;i++)
+//                 cities[i]+=i-(l-1);
+//         }
+//         for(int i=1;i<=n;i++)
+//         {
+//             printf("%d ", cities[i]);
+//             cities[i] = 0;
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// int main()
+// {
+//     int t,n,k;
+//     cin>>t;
+//     while(t--)
+//     {
+//         cin>>n>>k;
+//         int temp = k/n;
+//         cout<<temp<<endl;
+//     }
+//     return 0;
+// }
 
 // #include<bits/stdc++.h>
 

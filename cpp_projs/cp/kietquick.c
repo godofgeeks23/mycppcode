3fbd05ebf888ecdd9192fe59b0484c13cdcc00ec
Aviral Srivastava <aviralji4@gmail.com>
2021-01-09 18:32:39 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -3,5 +3,17 @@
 #include<stdlib.h>
 void main()
 {
-	printf("Hi...");
+	int t, n, tmp;
+	scanf("%d", &t);
+	while(t--)
+	{
+		scanf("%d", &n);
+		if(n%11==0)
+			tmp = n+2;
+		else if(n%4==0)
+			tmp = n-2;
+		else
+			tmp = n*4;
+		printf("%d\n", tmp);
+	}
 }

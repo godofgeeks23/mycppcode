78b99e90f86b5a89a47aee28b0223d9ae4d9aa8f
Aviral Srivastava <aviralji4@gmail.com>
2021-02-04 23:27:05 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -1,21 +1,158 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
+#include<string.h>
 void main() 
 {
-    int arr[10];
-    int n, sum, a, b;
-    scanf("%d", &n);
-    for(int i=0;i<n;i++)
-        scanf("%d", &arr[i]);
-    sum = abs(arr[0]+arr[1]);
-    for(int i=0;i<n;i++)
-        for(int j=i+1;j<n;j++)
-            if(abs(arr[i]+arr[j])<sum)
-            {
-                sum = abs(arr[i]+arr[j]);
-                a = arr[i];
-                b = arr[j];
-            }
-    printf("%d %d", a, b);
+    long int x, y, fibs[55];
+    int t, count;
+    fibs[0] = 1;
+    fibs[1] = 2;
+    scanf("%d", &t);
+    for(int i=2;i<55;i++)
+    {    
+        fibs[i] = fibs[i-1]+fibs[i-2];
+        printf("%lld\n", fibs[i]);
+    }
+    while(t--)
+    {
+        scanf("%lld %lld", &x, &y);
+        count = 0;
+        for(int i=0;i<55;i++)
+            if(fibs[i]>x && fibs[i]<y)
+                count++;
+        printf("%d\n", count);
+    }
 }
+
+// #include<stdio.h>
+// #include<math.h>
+// #include<stdlib.h>
+// #include<string.h>
+// long long int mygcd(long long int a, long long int b)
+// {
+//     if (b == 0)
+//         return a;
+//     return mygcd(b, a % b); 
+// }
+// void main() 
+// {
+//     int t, n, r;
+//     scanf("%d", &t);
+//     while(t--)
+//     {
+//         scanf("%d %d", &n, &r);
+//         long long int temp1 = 1;
+//         long long int temp2 = 1;
+//         if((n-r)<r)
+//             r = n-r;
+//         while(r)
+//         {
+//             temp1*=n;
+//             temp2*=r;
+//             n--;
+//             r--;
+//             long long int avi = mygcd(temp1, temp2);
+//             temp1/=avi;
+//             temp2/=avi;
+//         }
+//         printf("%lld\n", temp1);
+//     }
+// }
+
+// #include<stdio.h>
+// #include<math.h>
+// #include<stdlib.h>
+// #include<string.h>
+// void main() 
+// {
+//     int t, n, count;
+//     scanf("%d", &t);
+//     if(!t)
+//         printf("Enter atleast one attempt\n");
+//     else if(t>=100000)
+//         printf("Too many attempts\n");
+//     else
+//     {
+//         while(t--)
+//         {
+//             scanf("%d", &n);
+//             if(n<100000)
+//             {
+//                 count = 1;
+//                 for(int i=2;i<=(n/2);i++)
+//                     if(!(n%i))
+//                         count++;
+//                 if(n!=1)
+//                     count++;
+//                 printf("%d\n", count);
+//             }
+//             else
+//                 printf("number is too large\n");
+//         }
+//     }
+// }
+
+
+
+// #include<stdio.h>
+// #include<math.h>
+// #include<stdlib.h>
+// #include<string.h>
+// void main() 
+// {
+//     int k, n, t, sum;
+//     scanf("%d", &t);
+//     while(t--)
+//     {
+//         scanf("%d%d", &k, &n);
+//         sum = 0;
+//         for(int i=1;i<=n;i++)
+//         {
+//              sum+=i%k;   
+//         }
+//         printf("%d\n", sum);
+//     }
+// }
+
+// #include<stdio.h>
+// #include<math.h>
+// #include<stdlib.h>
+// #include<string.h>
+// void main() 
+// {
+//     int t, sum;
+//     char num[20];
+//     scanf("%d", &t);
+//     while(t--)
+//     {
+//         scanf("%s", num);
+//         sum = 0;
+//         int size = strlen(num);
+//         // printf("%d\n", (num[0]-'0'));
+//         for(int i=0;i<size;i=i+2)
+//             sum+=(num[i]-'0');
+//         printf("%d\n", sum);
+//     }
+// }
+
+// #include<stdio.h>
+// #include<math.h>
+// #include<stdlib.h>
+// #include<string.h>
+// void main() 
+// {
+//     int t, sum;
+//     char num[20];
+//     scanf("%d", &t);
+//     while(t--)
+//     {
+//         scanf("%s", num);
+//         sum = 0;
+//         int size = strlen(num);
+//         // printf("%d\n", (num[0]-'0'));
+//         sum = (num[0]-'0');
+//         sum+=(num[size-1]-'0');
+//         printf("%d\n", sum);
+//     }
+// }
--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -2,18 +2,30 @@
 // using namespace std;
 // int main()
 // {
-//            
-//      return 0;
-// } 
+//     clock_t start, end;
+//     start = clock();
+            
+//     // code goes here!
+
+//     end = clock();  
+//     cout << "Time: "<<fixed<<double(end-start)/double(CLOCKS_PER_SEC)<<setprecision(5)<<" s"<<endl; 
+//     return 0;
+// }
 
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
+    clock_t start, end;
+    start = clock();
     // this program is to demonstrate how to use hashing in code to reduce time complexity of the program
     // Question - given a number between 1 and 20, and variable number of queries, print the factorial of the number input in each query
-    cin>>q;
-    
+    // cin>>q;
+        // Calculates the time taken 
+    // by the algorithm to execute 
+
+    end = clock();  
+    cout << "Time: "<<fixed<<double(end-start)/double(CLOCKS_PER_SEC)<<setprecision(5)<<" s"<<endl; 
     return 0;
 } 
 

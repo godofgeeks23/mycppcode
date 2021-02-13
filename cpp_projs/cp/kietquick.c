2a2257d8293061384d40d930ae95e2a40f646ae0
Aviral Srivastava <aviralji4@gmail.com>
2021-02-13 17:27:30 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -4,7 +4,26 @@
 // #include<string.h>
 // void main() 
 // {
-//     
+//     int t;
+//     scanf("%d", &t);
+//     while(t--)
+//     {
+        
+//     }
 // }
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+void main() 
+{
+    int t;
+    scanf("%d", &t);
+    while(t--)
+    {
+        
+    }
+}
 
 

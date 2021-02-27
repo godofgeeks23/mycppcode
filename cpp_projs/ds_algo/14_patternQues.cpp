bb594430f706e88b28d0100b688d1d030390b764
Aviral Srivastava <aviralji4@gmail.com>
2021-02-27 18:20:17 +0530

    "updated the code base..."

--- cpp_projs/ds_algo/14_patternQues.cpp
+++ cpp_projs/ds_algo/14_patternQues.cpp
@@ -80,6 +80,24 @@ int main()
     }
     cout<<endl<<endl;
 
-    
+    int i, j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=9;j++)
+            if(j>(6-i) && j<(4+i))
+                cout<<" ";
+            else
+                cout<<j;
+        cout<<endl;
+    }    
+    for(i=4;i>=1;i--)
+    {
+        for(j=1;j<=9;j++)
+            if(j>(6-i) && j<(4+i))
+                cout<<" ";
+            else
+                cout<<j;
+        cout<<endl;
+    }
     return 0;
 }

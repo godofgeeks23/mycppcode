d795f4611411b593131536aeebb135284fd25d08
Aviral Srivastava <aviralji4@gmail.com>
2021-04-10 21:03:33 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -18,12 +18,20 @@
 #include<math.h>
 #include<stdlib.h>
 #include<string.h>
-void main() 
+void str_c(int m) 
 {
-    int t;
-    scanf("%d", &t);
-    while(t--)
+    char c[500];
+    fgets(c,500,stdin);
+    int i=0;
+    for(i=0;i<m;i++)
     {
-        
+        printf("%c",c[i]);
     }
+}
+int main()
+{ 
+    int m;
+    scanf("%d",&m);
+    str_c(m);
+    return 0;
 }

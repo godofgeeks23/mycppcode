d766b9c39f71982a76eb2769ff4470795f3d6dd2
Aviral Srivastava <aviralji4@gmail.com>
2021-04-11 23:19:20 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -14,6 +14,34 @@
 //     }
 // }
 
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void str_r() {
+    char str[500], rev[500];
+    int i, j=0;
+    fgets(str, 500, stdin);
+    while(str[j]!='\0')
+        j++;
+    for(i=0;i<=j;i++)
+        rev[i] = str[j-1-i];
+    puts(rev);
+}
+
+int main()
+{
+    str_r();
+    return 0;
+}
+
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>

54d5b3d7a7c64676354835f86dd4e9d8b5dd1e5f
Aviral Srivastava <aviralji4@gmail.com>
2021-04-13 22:55:46 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -14,52 +14,17 @@
 //     }
 // }
 
-#include <assert.h>
-#include <ctype.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void str_r() {
-    char str[500], rev[500];
-    int i, j=0;
-    fgets(str, 500, stdin);
-    while(str[j]!='\0')
-        j++;
-    for(i=0;i<=j;i++)
-        rev[i] = str[j-1-i];
-    puts(rev);
-}
-
-int main()
-{
-    str_r();
-    return 0;
-}
 
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
 #include<string.h>
-void str_c(int m) 
+void main() 
 {
-    char c[500];
-    fgets(c,500,stdin);
-    int i=0;
-    for(i=0;i<m;i++)
+    int t;
+    scanf("%d", &t);
+    while(t--)
     {
-        printf("%c",c[i]);
+        
     }
-}
-int main()
-{ 
-    int m;
-    scanf("%d",&m);
-    str_c(m);
-    return 0;
 }

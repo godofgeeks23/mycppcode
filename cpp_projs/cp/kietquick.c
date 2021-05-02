88dcc16283055435a104cf6a9ada3babb4b73d4c
Aviral Srivastava <aviralji4@gmail.com>
2021-05-02 18:31:11 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -9,50 +9,59 @@
     
 // }
 
-#include <assert.h>
-#include <ctype.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-void str_c() {
-    char str1[500];
-    char str2[500];
-    fgets(str1, 500, stdin);
-    fgets(str2, 500, stdin);
-    int l1 = strlen(str1);
-    int l2 = strlen(str2);
-    str1[l1-1] = '\0';
-    l1 = strlen(str1);
-    int flag = 0;
-    printf("%s:%d,\n%s:%d\n", str1, l1, str2, l2);
-    if(l1==l2)
-    {
-        // printf("equal lengths...\n");
-        for(int i=0;i<l1;i++)
-            if(str1[i]!=str2[i])
-            {
-                // printf("found a non-matching character %c-%c...\n", str1[i], str2[i]);
-                flag = -1;
-                break;
-            }
-    }
-    else
-    {
-        flag = -1;
-        // printf("\nNot equal because of unequal lengths...");
-    }
-    if(flag==0)
-        printf("Strings are equal.");
-    else
-        printf("Strings are not equal.");
-}
-int main()
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+void main() 
 {
-    str_c();
-    return 0;
+    
 }
+
+// #include <assert.h>
+// #include <ctype.h>
+// #include <limits.h>
+// #include <math.h>
+// #include <stdbool.h>
+// #include <stddef.h>
+// #include <stdint.h>
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <string.h>
+// void str_c() {
+//     char str1[500];
+//     char str2[500];
+//     fgets(str1, 500, stdin);
+//     fgets(str2, 500, stdin);
+//     int l1 = strlen(str1);
+//     int l2 = strlen(str2);
+//     str1[l1-1] = '\0';
+//     l1 = strlen(str1);
+//     int flag = 0;
+//     printf("%s:%d,\n%s:%d\n", str1, l1, str2, l2);
+//     if(l1==l2)
+//     {
+//         // printf("equal lengths...\n");
+//         for(int i=0;i<l1;i++)
+//             if(str1[i]!=str2[i])
+//             {
+//                 // printf("found a non-matching character %c-%c...\n", str1[i], str2[i]);
+//                 flag = -1;
+//                 break;
+//             }
+//     }
+//     else
+//     {
+//         flag = -1;
+//         // printf("\nNot equal because of unequal lengths...");
+//     }
+//     if(flag==0)
+//         printf("Strings are equal.");
+//     else
+//         printf("Strings are not equal.");
+// }
+// int main()
+// {
+//     str_c();
+//     return 0;
+// }

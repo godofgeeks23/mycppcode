1f3b15a82c7a9c77e39a6be76ffb6e9eec81cc05
Aviral Srivastava <aviralji4@gmail.com>
2021-05-14 23:40:53 +0530

    "updated the code base..."

--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -25,20 +25,6 @@
 using namespace std;
 int main()
 {
-    clock_t start, end;
-    start = clock();
-            
-    // code goes here!
-
-    end = clock();  
-    cout << "\nTime: "<<fixed<<double(end-start)/double(CLOCKS_PER_SEC)<<setprecision(5)<<"s"; 
-    return 0;
-}
-
-#include<bits/stdc++.h>
-using namespace std;
-int main()
-{
     
     return 0;
 }

623ab278706eb3d3387e128f4c18105493d0a519
Aviral Srivastava <aviralji4@gmail.com>
2021-05-16 23:09:21 +0530

    "updated the code base..."

--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -1,4 +1,4 @@
-// boilerplate code - basic code acting as a base for all the other programs
+// boilerplate code 1 - basic code acting as a base for all the other programs
 // #include<bits/stdc++.h>
 // using namespace std;
 // int main()
@@ -13,6 +13,7 @@
 //     return 0;
 // }
 
+// boilerplate code 2 - basic code acting as a base for all the other programs
 // #include<bits/stdc++.h>
 // using namespace std;
 // int main()

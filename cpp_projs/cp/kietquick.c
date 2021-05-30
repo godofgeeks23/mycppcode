7c2aa7d766311d3d468eee6fbaa0387dee63b24b
Aviral Srivastava <aviralji4@gmail.com>
2021-05-30 23:56:08 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -13,15 +13,25 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n-1; i++)    
+    for (j = 0; j < n-i-1; j++)
+        if (arr[j] > arr[j+1])
+        {
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+}
 int main() {   
-    int arr[10];
-    for(int i=0;i<10;i++)
-    {
+    int arr[5];
+    for(int i=0;i<5;i++)
         scanf("%d", &arr[i]);
-        if(arr[i]==5)
-            printf("Student %d may attain wisdom in future. Congratulations...!", i+1);
-    }
-
+    bubbleSort(arr, 5);
+    for(int i=0;i<5;i++)
+        printf("%d,", arr[i]);
     return 0;
 }
 

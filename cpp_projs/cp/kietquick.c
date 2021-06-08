5c3d2a095292d386ac7e91eb6077663a7ddfda56
Aviral Srivastava <aviralji4@gmail.com>
2021-06-08 23:42:10 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -9,7 +9,21 @@
     
 // }
 
-
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<string.h>
+void main() 
+{
+    int i=5,*j;
+    j = &i;
+    printf("%d\n",&i);
+    printf("%d\n",j);
+    printf("%d\n",&j);
+    printf("%d\n",j);
+    printf("%d\n",*(&i));
+    printf("%d\n",*j);
+}
 
 // #include<stdio.h>
 // #include<math.h>

628f037e72b6b1ddac5e5eb8bfe322d6134afbf2
Aviral Srivastava <aviralji4@gmail.com>
2021-06-12 22:26:25 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -15,5 +15,9 @@
 #include<string.h>
 void main() 
 {
-    
+    int daily, coding , habit;
+    daily = coding = habit = 2;
+    habit = daily++||++coding && ++habit;
+    printf("%d%d%d", habit, daily, coding);
 }
+// end of program

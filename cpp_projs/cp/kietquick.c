5a8db44598d997501623c2c377d007f7f667c57a
Aviral Srivastava <aviralji4@gmail.com>
2021-06-15 22:57:56 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -1,23 +1,69 @@
-// boilerplate code - 
+// boilerplate code -
 // --------------------
 // #include<stdio.h>
 // #include<math.h>
 // #include<stdlib.h>
 // #include<string.h>
-// void main() 
+// void main()
 // {
-    
+
 // }
 
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
 #include<string.h>
-void main() 
+void main()
 {
-    int daily, coding , habit;
-    daily = coding = habit = 2;
-    habit = daily++||++coding && ++habit;
-    printf("%d%d%d", habit, daily, coding);
+    int a, b, c, sum;
+    scanf("%d%d%d", &a, &b, &c);
+    sum = calsum(a, b, c);
+    printf("%d", sum);
 }
-// end of program
+calsum(int x, int y, int z)
+{
+    int d;
+    d = y;
+    d = 56;
+    printf("%d\n", d);
+    // return d;
+}
+
+// #include<stdio.h>
+// #include<math.h>
+// #include<stdlib.h>
+// #include<string.h>
+// void main()
+// {
+//     int x = 5;
+//     // int y = (--x)+(--x)+(x=10);
+//     // printf("%d %d\n", x, y);
+//     int y = 
+// }
+
+// 6 = a
+// fact = 1x2x3x4x.......a
+// #include <stdio.h>
+// #include <math.h>
+// #include <stdlib.h>
+// #include <string.h>
+// void main()
+// {
+//     int a;
+//     char cont;
+//     do
+//     {
+//         scanf("%d", &a);
+//         int factorial = 1;
+//         int i = 1;
+//         while (i <= a)
+//         {
+//             factorial = factorial * i;
+//             // printf("now factorial is %d\n", factorial);
+//             i++;
+//         }
+//         printf("%d", factorial);
+//         printf("do you want to continue or not ? ");
+//         scanf("%c", &cont);
+//     } while (cont == 'y');
+// }
--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -21,14 +21,123 @@
     
 //     return 0;
 // }
+ 
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+const int MAX = 1000001;
+
+int factor[MAX] = { 0 };
+
+void generatePrimeFactors()
+{
+    factor[1] = 1;
+    for (int i = 2; i < MAX; i++)
+        factor[i] = i;
+    for (int i = 4; i < MAX; i += 2)
+        factor[i] = 2;
+    for (int i = 3; i * i < MAX; i++) {
+        if (factor[i] == i) {
+            for (int j = i * i; j < MAX; j += i) {
+                if (factor[j] == j)
+                    factor[j] = i;
+            }
+        }
+    }
+}
+int calculateNoOFactors(int n)
+{
+    if (n == 1)
+        return 1;
+
+    int ans = 1;
+
+    int dup = factor[n];
+
+    int c = 1;
+
+    int j = n / factor[n];
+
+    // false when prime factorization is done
+    while (j != 1) {
+        // if the same prime number is dividing n,
+        // then we increase the count
+        if (factor[j] == dup)
+            c += 1;
+
+        /* if its a new prime factor that is factorizing n,
+        then we again set c=1 and change dup to the new
+        prime factor, and apply the formula explained
+        above. */
+        else {
+            dup = factor[j];
+            ans = ans * (c + 1);
+            c = 1;
+        }
+
+        // prime factorizes a number
+        j = j / factor[j];
+    }
+
+    // for the last prime factor
+    ans = ans * (c + 1);
+
+    return ans;
+}
+
+// Driver program to test above function
 int main()
 {
-    cout<<"Hello World";
+    // generate prime factors of number
+    // upto 10^6
+    generatePrimeFactors();
+
+    int a;
+    cin>>a;
+    if(calculateNoOFactors(a)==3)
+        cout <<"YES";
+    else
+        cout<<"NO";
+
     return 0;
 }
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int t;
+//     cin>>t;
+//     int arr1[30], arr2[30], n, k, sum;
+//     while(t--)
+//     {
+//         cin>>n>>k;
+//         sum = 0;
+//         for(int i=0;i<n;i++)
+//         {
+//             cin>>arr1[i];
+//             sum+=arr1[i];
+//         }
+//         for(int i=0;i<n;i++)
+//             cin>>arr2[i];
+//         sort(arr1, arr1+n, greater<int>());
+//         sort(arr2, arr2+n, greater<int>());
+//         int ptr1 = n-1;
+//         int ptr2 = 0;
+//         while(k>0 && arr1[ptr1]<arr2[ptr2])
+//         {
+//             sum = sum - arr1[ptr1];
+//             sum = sum + arr2[ptr2];
+//             ptr1--;
+//             ptr2++;
+//             k--;
+//         }
+//         cout<<sum;
+//     }
+//     return 0;
+// }
 
 
 // #include<bits/stdc++.h>

316a430262c64a1a2b2ef3fbbf6ae39325d99ad8
Aviral Srivastava <aviralji4@gmail.com>
2021-06-21 18:45:07 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -9,6 +9,66 @@
 
 // }
 
+// #include<stdio.h>
+// #include<math.h>
+// #include<stdlib.h>
+// #include<string.h>
+// void main()
+// {
+//     int l, k, sum;
+//     scanf("%d %d", &l, &k);
+//     sum = k;
+//     l--;
+//     while(l--)
+//     {
+//         k *= k;
+//         sum+=k;
+//     }
+//     printf("%d\n", sum);
+// }
+
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+int mult(int x, int y)
+{
+    if (y != 0)
+        return (x + mult(x, y - 1));
+    else
+        return 0;
+}
+int main()
+{
+    int x = 5, y = 60;
+    printf("%d",mult(x, y));
+    return 0;
+}
+
+// #include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
+// void shiftrings(int n, char from, char to, char extra)
+// {
+//     if (n == 1)
+//     {
+//         printf("Moving ring 1 from %c to %c\n\n", from, to);
+//         return;
+//     }
+//     shiftrings(n - 1, from, extra, to);
+//     printf("Moving ring %d from %c to %c\n\n", n, from, to);
+//     shiftrings(n - 1, extra, to, from);
+// }
+// int main() {
+
+//     int n;
+//     scanf("%d", &n);
+//     shiftrings(n, 'A', 'C', 'B');    
+//     return 0;
+// }
+ 
+// This is code is contributed by rathbhupendra
+
 // #include <stdio.h>
 // #include <math.h>
 // #include <stdlib.h>
--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -22,60 +22,14 @@
 //     return 0;
 // }
  
-
-#include <bits/stdc++.h>
+boilerplate code 2 - basic code acting as a base for all the other programs
+#include<bits/stdc++.h>
 using namespace std;
-const int MAX = 1000001;
-int factor[MAX] = { 0 };
-void generatePrimeFactors()
-{
-    factor[1] = 1;
-    for (int i = 2; i < MAX; i++)
-        factor[i] = i;
-    for (int i = 4; i < MAX; i += 2)
-        factor[i] = 2;
-    for (int i = 3; i * i < MAX; i++) {
-        if (factor[i] == i) {
-            for (int j = i * i; j < MAX; j += i) {
-                if (factor[j] == j)
-                    factor[j] = i;
-            }
-        }
-    }
-}
-int calculateNoOFactors(int n)
-{
-    if (n == 1)
-        return 1;
-    int ans = 1;
-    int dup = factor[n];
-    int c = 1;
-    int j = n / factor[n];
-    while (j != 1) {
-        if (factor[j] == dup)
-            c += 1;
-        else {
-            dup = factor[j];
-            ans = ans * (c + 1);
-            c = 1;
-        }
-        j = j / factor[j];
-    }
-    ans = ans * (c + 1);
-    return ans;
-}
 int main()
 {
-    generatePrimeFactors();
-    int a;
-    cin>>a;
-    if(calculateNoOFactors(a)==3)
-        cout <<"YES";
-    else
-        cout<<"NO";
+    
     return 0;
 }
-
 
 // #include<bits/stdc++.h>
 // using namespace std;

491f34d2953967c221ee3595689ebee718cb4995
Aviral Srivastava <aviralji4@gmail.com>
2021-09-03 22:13:45 +0530

    "updated the code base..."

--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -30,7 +30,16 @@ using namespace std;
 int main()
 {
     // your code goes here
-
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+            cout<<" ";
+        for(int j=0;j<i+1;j++)
+            cout<<"#";
+        cout<<endl;
+    }
     return 0;
 }
 

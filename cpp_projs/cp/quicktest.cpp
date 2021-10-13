e5d28c4bf2a939cab2b2677b0f10096a7901f90b
Aviral Srivastava <aviralji4@gmail.com>
2021-10-13 22:20:57 +0530

    "updated the code base..."

--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -28,6 +28,17 @@
 using namespace std;
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // code goes here!
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
     
     return 0;
 }

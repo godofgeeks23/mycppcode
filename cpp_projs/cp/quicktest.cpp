1883510b1aac6073cf4482ed1c164298db6d1339
Aviral Srivastava <aviralji4@gmail.com>
2021-10-28 23:22:33 +0530

    "updated the code base..."

--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -24,21 +24,43 @@
 //     return 0;
 // }
 
-#include<bits/stdc++.h>
-using namespace std;
-int main()
+#include <stdio.h>
+int search(int arr[], int n, int x)
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    
+    int i;
+    for (i = 0; i < n; i++)
+        if (arr[i] == x)
+            return i;
+    return -1;
+}
+int main(void)
+{
+    int arr[] = { 2, 3, 4, 10, 40 };
+    int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int result = search(arr, n, x);
+    if (result == -1)
+        printf("Element is not present in array");
+    else
+        printf("Element is present at index %d", result);
     return 0;
 }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//     cout.tie(NULL);
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++)
+//         cin>>arr[i];
+    
+//     return 0;
+// }
 
 // #include<bits/stdc++.h>
 // using namespace std;

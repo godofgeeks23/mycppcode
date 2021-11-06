d61d87f1c1b459097cdb0b0be3522065c403a1fb
Aviral Srivastava <aviralji4@gmail.com>
2021-11-06 23:34:01 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -7,22 +7,3 @@
 // {
 
 // }
-
-#include<stdio.h>
-void TOH(int n, char a, char b, char c)
-{
-    if (n == 1)
-    {
-        printf("\nMove disk 1 from %c to %c", a, c);
-        return;
-    }
-    TOH(n - 1, a, c, b);
-    printf("\nMove disk %d from %c to %c", n, a, c);
-    TOH(n - 1, b, a, c);
-}
-int main() 
-{
-    int n = 3;
-    TOH(n, 'A', 'B', 'C');
-    return 0;
-}

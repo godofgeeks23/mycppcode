c2f3c2f3b7f30840defb22ffaa8e11b6b966d367
Aviral Srivastava <aviralji4@gmail.com>
2021-11-19 23:47:41 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -13,58 +13,7 @@
 #include<math.h>
 #include<stdlib.h>
 #include<string.h>
-struct node
+void main()
 {
-    int data;
-    struct node *next;
-};
-struct node *top = NULL;
-void push(int x)
-{
-    struct node *temp = (struct node *)malloc(sizeof(struct node));
-    temp->data = x;
-    temp->next = top;
-    top = temp;
-}
-int pop()
-{
-    if(top == NULL)
-    {
-        printf("Stack is empty\n");
-        return -1;
-    }
-    struct node *temp = top;
-    top = top->next;
-    int x = temp->data;
-    free(temp);
-    return x;
-}
-void display()
-{
-    struct node *temp = top;
-    while(temp != NULL)
-    {
-        printf("%d ",temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-int main()
-{
-    int choice = 1, element;
-    while(choice)
-    {
-        printf("Enter 0: quit\t1: push\t2: pop\t3: display\n");
-        scanf("%d", &choice);
-        switch(choice)
-        {
-            case 1:
-                scanf("%d", &element);
-                push(element); break;
-            case 2: printf("Popped %d\n", pop()); break;
-            case 3: display(); break;
-            default: exit(0);
-        }
-    }
-    return 0;
+
 }

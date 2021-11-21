8a3a2575a9442b99bdabe21f5744fffc3372a119
Aviral Srivastava <aviralji4@gmail.com>
2021-11-21 23:01:43 +0530

    "updated the code base..."

--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -24,57 +24,21 @@
 //     return 0;
 // }
 
-// #include <iostream>
-// using namespace std;
-
-// int main(int argc, char **argv){
-//     int n, i, j;
-//     cin >> n;
-
-//     for(i=(n+1)/2;i>=1;i--)
-//     {
-
-//         for(j=1;j<=i;j++)
-//             cout<<"*\t";
-//         for(j=1;j<=(n+1)/2-i+1;j++)
-//             cout<<"\t";
-//         for(j=1;j<=i;j++)
-//             cout<<"*\t";
-//         cout<<endl;
-//     }
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    clock_t start, end;
+    start = clock();
+    // this program is to demonstrate how to use hashing in code to reduce time complexity of the program
+    // Question - given a number between 1 and 20, and variable number of queries, print the factorial of the number input in each query
+    // cin>>q;
+        // Calculates the time taken
+    // by the algorithm to execute
     
-// }
 
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main()
-// {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(NULL);
-//     cout.tie(NULL);
-//     int n;
-//     cin>>n;
-//     int arr[n];
-//     for(int i=0;i<n;i++)
-//         cin>>arr[i];
-    
-//     return 0;
-// }
-
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main()
-// {
-//     clock_t start, end;
-//     start = clock();
-//     // this program is to demonstrate how to use hashing in code to reduce time complexity of the program
-//     // Question - given a number between 1 and 20, and variable number of queries, print the factorial of the number input in each query
-//     // cin>>q;
-//         // Calculates the time taken
-//     // by the algorithm to execute
-
-//     end = clock();
-//     cout << "Time: "<<fixed<<double(end-start)/double(CLOCKS_PER_SEC)<<setprecision(5)<<" s"<<endl;
-//     return 0;
-// }
+    end = clock();
+    cout << "Time: "<<fixed<<double(end-start)/double(CLOCKS_PER_SEC)<<setprecision(5)<<" s"<<endl;
+    return 0;
+}
 

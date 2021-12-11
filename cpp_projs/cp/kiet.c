4bfaeb6280ebee218c823a07a576be74f9d0c4dd
Aviral Srivastava <aviralji4@gmail.com>
2021-12-11 22:41:37 +0530

    "updated the code base..."

--- cpp_projs/cp/kiet.c
+++ cpp_projs/cp/kiet.c
@@ -1,86 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define MAX 5
-int front = -1;
-int rear = -1;
-int queue[MAX];
-void enqueue();
-void dequeue();
-void display();
+#include<stdio.h>
+#include<ctype.h>
+
+char stack[100];
+int top = -1;
+
+void push(char x)
+{
+    stack[++top] = x;
+}
+
+char pop()
+{
+    if(top == -1)
+        return -1;
+    else
+        return stack[top--];
+}
+
+int priority(char x)
+{
+    if(x == '(')
+        return 0;
+    if(x == '+' || x == '-')
+        return 1;
+    if(x == '*' || x == '/')
+        return 2;
+    return 0;
+}
+
 int main()
 {
-	int choice;
-	while (1)
-	{
-		printf("\npress 1:enqueue 2:dequeue 3:display 0:exit\n");
-		printf("enter choice:");
-		scanf("%d", &choice);
-		switch (choice)
-		{
-		case 1:
-			enqueue();
-			break;
-		case 2:
-			dequeue();
-			break;
-		case 3:
-			display();
-			break;
-		case 0:
-			exit(1);
-			break;
-		default:
-			printf("wrong input\n");
-		}
-	}
-}
-void enqueue()
-{
-	if (rear == MAX - 1)
-	{
-		printf("Queue is full\n");
-	}
-	else
-	{
-		if (front == -1)
-		{
-			front = 0;
-		}
-		rear = (rear + 1) % MAX;
-		printf("enter the element to enqueue:");
-		scanf("%d", &queue[rear]);
-	}
-}
-void dequeue()
-{
-	if (front == -1)
-	{
-		printf("queue is empty\n");
-	}
-	else if (front == rear)
-	{
-		printf("dequeue element = %d", queue[front]);
-		rear = front = -1;
-	}
-	else
-	{
-		printf("dequeue element = %d", queue[front]);
-		front = (front + 1) % MAX;
-	}
-}
-void display()
-{
-	int i;
-	if (front == -1 || front >= rear)
-	{
-		printf("queue is empty\n");
-	}
-	else
-	{
-		for (i = front; i != rear; i = (i + 1) % MAX)
-		{
-			printf("%d ", queue[i]);
-		}
-		printf("%d", queue[i]);
-	}
+    char exp[100];
+    char *e, x;
+    printf("Enter the expression : ");
+    scanf("%s",exp);
+    printf("\n");
+    e = exp;
+    
+    while(*e != '\0')
+    {
+        if(isalnum(*e))
+            printf("%c ",*e);
+        else if(*e == '(')
+            push(*e);
+        else if(*e == ')')
+        {
+            while((x = pop()) != '(')
+                printf("%c ", x);
+        }
+        else
+        {
+            while(priority(stack[top]) >= priority(*e))
+                printf("%c ",pop());
+            push(*e);
+        }
+        e++;
+    }
+    
+    while(top != -1)
+    {
+        printf("%c ",pop());
+    }return 0;
 }

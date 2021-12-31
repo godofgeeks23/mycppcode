dd7a175b81e7c57554e4d1bcf1aa04a9dd0f3e64
Aviral Srivastava <aviralji4@gmail.com>
2021-12-31 22:19:48 +0530

    updated the code base...

---
+++ cpp_projs/cp/copilottest.cpp
@@ -0,0 +1,17 @@
+// this is a code file to test the code that github copilot generates
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<string> v;
+    v.push_back("hello");
+    v.push_back("world");
+    v.push_back("!");
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << endl;
+    }
+    return 0;
+}

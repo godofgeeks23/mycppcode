1ac93dfe5a2e97a21c214d75f2c6cd35c48e8ed2
Aviral Srivastava <aviralji4@gmail.com>
2022-01-16 21:41:27 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -21,185 +21,54 @@ Code, Compile, Run and Debug online from anywhere in world.
 *******************************************************************************/
 /* C Program for the implentation of Restoring Division
 */
-#include<stdio.h>
-#include<conio.h>
-#include<math.h>
 
-int getsize(int x)
+#include <stdio.h>
+#include <stdlib.h>
+ 
+void indexedSequentialSearch(int arr[], int n, int k)
 {
-  int c;
-  if(x<=1)
-  c = 2;
-  else if(x < 4)
-  c = 2;
-  else if(x< 8)
-  c = 3;
-  else if(x< 16)
-  c = 4;
-  else if(x< 32)
-  c = 5;
-  else if(x< 64)
-  c = 6;
-  else if(x< 128)
-  c = 7;
-  else if(x< 256)
-  c = 8;
-  else if(x< 512)
-  c = 9;
-  return c;
+    int elements[20], indices[20], temp, i, set = 0;
+    int j = 0, ind = 0, start, end;
+    for (i = 0; i < n; i += 3) {
+        elements[ind] = arr[i];
+        indices[ind] = i;
+        ind++;
+    }
+    if (k < elements[0]) {
+        printf("Not found");
+        exit(0);
+    }
+    else {
+        for (i = 1; i <= ind; i++)
+            if (k <= elements[i]) {
+                start = indices[i - 1];
+                end = indices[i];
+                set = 1;
+                break;
+            }
+    }
+    if (set == 0) {
+        start = indices[i - 1];
+        end = n;
+    }
+    for (i = start; i <= end; i++) {
+        if (k == arr[i]) {
+            j = 1;
+            break;
+        }
+    }
+    if (j == 1)
+        printf("Found at index %d", i);
+    else
+        printf("Not found");
 }
-
-int max(int x,int y)
-{
-  if(x< y)
-  return(y);
-  else
-  return(x);
-}
-
 void main()
 {
-  int B,Q,Z,M,c,c1,e,f,g,h,i,j,x,y,ch,in,S,G,P;
-  int a[24],b[12],b1[12],q[12],carry=0,count=0,option;
-  long num;
-  do
-  {
-  // clrscr();
-  printf("¦-----------------------------------------------¦\n");
-  printf("¦\t\tPROGRAM FOR DIVISION\t\t¦\n");
-  printf("¦-----------------------------------------------¦");
-  printf("\n\nENTER DIVIDEND\t: ");
-  scanf("%d",&Q);
-  y = getsize(Q);
-  printf("ENTER DIVISOR\t: ");
-  scanf("%d",&M);
-  x = getsize(M);
-  Z = max(x,y);
-  printf("\n\tTOTAL BITS CONSIDERED FOR RESULT => %d",2*Z+1);
-  printf("\n\tINITiALLY A IS RESET TO ZERO:");
-  for(i=0;i<=Z;i++)
-  printf("%d ",a[i]=0);
-  for(i=Z;i>=0;i--)
-  {
-  b1[i] = b[i] = M%2;
-  M = M/2;
-  b1[i] = 1-b1[i];
-  }
-  carry = 1;
-  for(i=Z;i>=0;i--)
-  {
-  c1 = b1[i]^carry;
-  carry = b1[i]&&carry;
-  b1[i]=c1;
-  }
-  for(i=2*Z;i>Z;i--)
-  {
-  a[i] = Q%2;
-  Q = Q/2;
-  }
-  printf("\n\n\tDivisor\t\t(M)\t: ");
-  for(i=0;i<=Z;i++)
-  printf("%d ",b[i]);
-  printf("\n\t2'C Divisor\t(M)\t: ");
-  for(i=0;i<=Z;i++)
-  printf("%d ",b1[i]);
-  printf("\n\tDividend\t(Q)\t: ");
-  for(i=Z+1;i<=2*Z;i++)
-  printf("%d ",a[i]);
-  printf("\n\n\tBITS CONSIDERED:[ A ] [ M ]");
-  printf("\n\t\t\t");
-  for(i=0;i<=Z;i++)
-  printf("%d ",a[i]);
-  printf(" ");
-  for(i=Z+1;i<=2*Z;i++)
-  printf("%d ",a[i]);
-  count = Z;
-  do{
-  for(i=0;i< 2*Z;i++)
-  a[i] = a[i+1];
-  printf("\n\nLeft Shift\t\t");
-  for(i=0;i<=Z;i++)
-  printf("%d ",a[i]);
-  printf(" ");
-  for(i=Z+1;i< 2*Z;i++)
-  printf("%d ",a[i]);
-  carry=0;
-  for(i=Z;i>=0;i--)
-  {
-  S=a[i]^(b1[i]^carry);
-  G=a[i]&&b1[i];
-  P=a[i]^b1[i];
-  carry=G||(P&&carry);
-  a[i]=S ;
-  }
-  printf("\nA< -A-M \t\t");
-  for(i=0;i<=Z;i++)
-  printf("%d ",a[i]);
-  printf(" ");
-  for(i=Z+1;i< 2*Z;i++)
-  printf("%d ",a[i]);
-  ch=a[0];
-  printf("\nBIT Q:%d",ch);
-  switch (ch)
-  {
-  case 0: a[2*Z]=1;
-  printf(" Q0< -1\t\t");
-  for(i=0;i<=Z;i++)
-  printf("%d ",a[i]);
-  printf(" ");
-  for(i=Z+1;i<=2*Z;i++)
-  printf("%d ",a[i]);
-  break;
+ 
+    int arr[] = { 6, 7, 8, 9, 10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-  case 1: a[2*Z]=0;
-  printf(" Q0< -0\t\t");
-  for(i=0;i<=Z;i++)
-  printf("%d ",a[i]);
-  printf(" ");
-  for(i=Z+1;i< 2*Z;i++)
-  printf("%d ",a[i]);
-  carry=0;
-  for(i=Z;i>=0;i--)
-  {
-  S=a[i]^(b[i]^carry);
-  G=a[i]&&b[i];
-  P=a[i]^b[i];
-  carry=G||(P&&carry);
-  a[i]=S ;
-  }
-  printf("\nA< -A+M");
-  printf("\t\t\t");
-  for(i=0;i<=Z;i++)
-  printf("%d ",a[i]);
-  printf(" ");
-  for(i=Z+1;i<=2*Z;i++)
-  printf("%d ",a[i]);
-  break;
-  }
-  count--;
-  }while(count!=0);
-  num=0;
-  printf("\n\t\t< < QUOTIENT IN BITS>> :");
-  for(i=Z+1;i<=2*Z;i++)
-  {
-  printf("%d ",a[i]);
-  num=num+pow(2,2*Z-i)*a[i];
-  }
-  printf("\n\t\tOUOTIENT IN DECIMAL :%d",num);
-  num=0;
-  printf("\n\t\t< < REMAINDER IN BITS>>:");
-  for(i=0;i<=Z;i++)
-  {
-  printf("%d ",a[i]);
-  num=num+pow(2,Z-i)*a[i];
-  }
-  printf("\n\t\tREMAINDER IN DECIMAL :%d",num);
-  getche();
-  printf("\n\tDO YOU WANT TO CONTINUE PRESS 0-ESC 1-CONT.:");
-  scanf("%d",&option);
-
-
-
-  }while(option!=0);
-// getch();
+    int k = 8;
+    indexedSequentialSearch(arr, n, k);
 }
+
--- cpp_projs/cp/temp.cpp
+++ cpp_projs/cp/temp.cpp
@@ -80,35 +80,53 @@
 //     return 0;
 // }
 
-#include <bits/stdc++.h>
-using namespace std;
-
-void solve()
+#include <stdio.h>
+#include <stdlib.h>
+ 
+void indexedSequentialSearch(int arr[], int n, int k)
 {
-    int n, x;
-    cin>>n>>x;
-    int arr[n];
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    sort(arr, arr+n, greater<int>());
-    int i, sum = 0;
-    for(i=0;i<n;i++)
-    {
-        sum+=arr[i];
-        if(sum>=x)
-        {
-            cout<<i+1<<endl;
+    int elements[20], indices[20], temp, i, set = 0;
+    int j = 0, ind = 0, start, end;
+    for (i = 0; i < n; i += 3) {
+        elements[ind] = arr[i];
+        indices[ind] = i;
+        ind++;
+    }
+    if (k < elements[0]) {
+        printf("Not found");
+        exit(0);
+    }
+    else {
+        for (i = 1; i <= ind; i++)
+            if (k <= elements[i]) {
+                start = indices[i - 1];
+                end = indices[i];
+                set = 1;
+                break;
+            }
+    }
+    if (set == 0) {
+        start = indices[i - 1];
+        end = n;
+    }
+    for (i = start; i <= end; i++) {
+        if (k == arr[i]) {
+            j = 1;
             break;
         }
     }
-    if(i==n)
-        cout<<-1<<endl;
+    if (j == 1)
+        printf("Found at index %d", i);
+    else
+        printf("Not found");
 }
-int main()
+void main()
 {
-	int t;
-	cin >> t;
-	while (t--)
-		solve();
-	return 0;
+ 
+    int arr[] = { 6, 7, 8, 9, 10 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int k = 8;
+    indexedSequentialSearch(arr, n, k);
 }
+

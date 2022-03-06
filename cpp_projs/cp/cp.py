30a3367a5c5461783f167dfa9cbead03835a078d
Aviral Srivastava <aviralji4@gmail.com>
2022-03-06 23:50:28 +0530

    "updated the code base..."

--- cpp_projs/cp/cp.py
+++ cpp_projs/cp/cp.py
@@ -1,14 +1,14 @@
 def shortestToChar(self, S, C):
-        index_nums=[]
-        for i, letter in enumerate(s):
-            if letter == s: 
-                index_nums.append(i)
-        ans = []
-        l=len(s)
-        l2=len(index_nums)
-        d=[None]*l2
-        for j in range(l):
-            for k in range(l2):
-                d[k]=abs(j-index_nums[k])
-            ans.append(min(d))
-        return ans
+    index_nums=[]
+    for i, letter in enumerate(s):
+        if letter == s: 
+            index_nums.append(i)
+    ans = []
+    l=len(s)
+    l2=len(index_nums)
+    d=[None]*l2
+    for j in range(l):
+        for k in range(l2):
+            d[k]=abs(j-index_nums[k])
+        ans.append(min(d))
+    return ans

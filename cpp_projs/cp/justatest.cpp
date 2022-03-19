fa68bca680b2518d535a0dfd6798fafb325cd7d9
Aviral Srivastava <aviralji4@gmail.com>
2022-03-19 17:44:41 +0530

    "updated the code base..."

--- cpp_projs/cp/justatest.cpp
+++ cpp_projs/cp/justatest.cpp
@@ -1,11 +1,42 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
+        int n;
+        cin >> n;
+        int a[n], b[n];
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        for (int i = 0; i < n; i++)
+            cin >> b[i];
+
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                if (a[j] > a[j + 1])
+                {
+                    swap(&a[j], &a[j + 1]);
+                    swap(&b[j], &b[j + 1]);
+                }
+
+        int count = 0;
+        int ptr = 0;
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (a[i] < a[i + 1] && b[i] < b[i + 1])
+                count++;
         }
+        int ans = n - count;
+        cout << ans;
+    }
 }

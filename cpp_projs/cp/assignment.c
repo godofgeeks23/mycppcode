c6c1e83d194c77912cd994950bc57e4506b08ad8
Aviral Srivastava <aviralji4@gmail.com>
2022-04-16 22:39:27 +0530

    "updated the code base..."

--- cpp_projs/cp/assignment.c
+++ cpp_projs/cp/assignment.c
@@ -11,12 +11,12 @@ void selectionSort(int arr[], int n)
 {
 	int i, j, min_idx;
 
-	for (i = 0; i < n-1; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		min_idx = i;
-		for (j = i+1; j < n; j++)
-		if (arr[j] < arr[min_idx])
-			min_idx = j;
+		for (j = i + 1; j < n; j++)
+			if (arr[j] < arr[min_idx])
+				min_idx = j;
 
 		swap(&arr[min_idx], &arr[i]);
 	}
@@ -24,7 +24,7 @@ void selectionSort(int arr[], int n)
 void printArray(int arr[], int size)
 {
 	int i;
-	for (i=0; i < size; i++)
+	for (i = 0; i < size; i++)
 		printf("%d ", arr[i]);
 	printf("\n");
 }
@@ -32,7 +32,7 @@ void printArray(int arr[], int size)
 int main()
 {
 	int arr[] = {64, 25, 12, 22, 11};
-	int n = sizeof(arr)/sizeof(arr[0]);
+	int n = sizeof(arr) / sizeof(arr[0]);
 	selectionSort(arr, n);
 	printf("Sorted array: \n");
 	printArray(arr, n);

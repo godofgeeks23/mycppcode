0b08b8ec9e3f8d231c9b267f5255bc538a324ba0
Aviral Srivastava <aviralji4@gmail.com>
2022-05-14 18:50:37 +0530

    "updated the code base..."

--- cpp_projs/cp/justatest.cpp
+++ cpp_projs/cp/justatest.cpp
@@ -1,14 +1,32 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <unordered_map>
 using namespace std;
-
+long long int hackersum(int hack[], int m)
+{
+    int hacksum = 0;
+    unordered_map<int, int> mp;
+    for(int i=0;i<=m;i++)
+    {
+        int s= 0;
+        for(int j=i;j<=m;j++)
+        {
+            s += hack[j];
+            mp[s]++;
+        }
+    }
+    for(auto x:mp)
+    {
+        if(x.second == 1)
+        {
+            hacksum += x.first;
+        }
+    }
+    return hacksum;
+}
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        
-    }
+    int arr[] = {3,6,6,7,8};
+    cout<<hackersum(arr, 5);
+
+    return 0;
 }

91cadd8965a7772e2fbda21a72156b3d157f85a5
Aviral Srivastava <aviralji4@gmail.com>
2022-05-19 22:48:08 +0530

    "updated the code base..."

--- cpp_projs/cp/cp.py
+++
@@ -1,8 +0,0 @@
-sum = 0
-print("The first n positive numbers having the total sum less than 100 are:")
-for i in range(1, 100):
-    while((sum+i) < 100):
-        sum += i
-        print(i, end=" ")
-        break
-print("Sum of numbers = ", sum)

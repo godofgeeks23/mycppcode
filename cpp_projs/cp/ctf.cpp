f15e322c6517fd3d05eab9ad5a01beb3f5bd2263
Aviral Srivastava <aviralji4@gmail.com>
2022-05-21 23:00:40 +0530

    "updated the code base..."

--- cpp_projs/cp/ctf.cpp
+++ cpp_projs/cp/ctf.cpp
@@ -1,5 +1,9 @@
 import zipfile
-    num = 15323 while (num >= 0) :filename = "{score}" + str(num)
-                                                                 with zipfile.ZipFile(filename, "r") as zip_ref:zip_ref.extractall()
-                                                                                                    print("extracting file:", filename)
-                                                                                                              num -= 1
+num = 15323 
+while (num >= 0):
+filename = "{score}" + str(num)
+with zipfile.ZipFile(filename, "r") as zip_ref:
+    zip_ref.extractall()
+    print("extracting file:", filename)
+    num -= 1
+print("done")

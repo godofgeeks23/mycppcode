5868b0bf575ef9160a98218dd89ed7f0a7bfa4cc
Aviral Srivastava <aviralji4@gmail.com>
2022-05-27 22:50:04 +0530

    "updated the code base..."

--- cpp_projs/cp/learnCPPbasics.cpp
+++ cpp_projs/cp/learnCPPbasics.cpp
@@ -5,15 +5,6 @@ using namespace std;
 
 int main()
 {
-    int i = 1,
-     k = i&0;
-     do
-     {
-         k++;
-         if(k>1)
-         i = k;
-     } while (i<2);
-     
-cout<<k;
+    
     return 0;
 }

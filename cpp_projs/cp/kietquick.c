3d490f980e39d98c7ed27a388014d9a049ebe938
Aviral Srivastava <aviralji4@gmail.com>
2022-05-30 23:04:27 +0530

    "updated the code base..."

--- cpp_projs/cp/kietquick.c
+++ cpp_projs/cp/kietquick.c
@@ -9,15 +9,11 @@
 
 // }
 
-/* C Program for the implentation of Restoring Division
- */
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby,
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-/* C Program for the implentation of Restoring Division
- */
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+void main()
+{
+    
+}

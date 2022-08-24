933cdc3d099a527f44683990a15a7cdc284bd30a
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-08-24 10:53:54 +0530

    "updated the code base"

--- cpp_projs/cp/cp.py
+++ cpp_projs/cp/cp.py
@@ -1,29 +1,3 @@
-# # def check(num):
-# #     num = str(num)
-# #     if (int(num) < 20 and int(num)%2 == 0) or (len(num) ==2 and int(num)%11 == 0):
-# #         return 1
-# #     if len(num) <= 2 and int(num)%2 != 0:
-# #         return 0
-# #     A = num[0]
-# #     B = num[1]
-# #     remaining = num[2:-2]
-# #     Y = num[-2]
-# #     Z = num[-1]
-# #     if A == '1':
-# #         if B == Z:
-# #             check(remaining + (str(int(Y)-1) if Y != '0' else '9'))
-# #         elif int(B)-1 == int(Z):
-# #             check('1' + remaining + (str(int(Y)-1) if Y != '0' else '9'))
-# #         else:
-# #             return 0
-# #     else:
-# #         if A == Z:
-# #             check(B + remaining + Y)
-# #         elif int(A) - 1 == int(Z):
-# #             check('1' + B + remaining + Y)
-# #         else:
-# #             return 0
-
 # count = 0
 # for num in input1:
 

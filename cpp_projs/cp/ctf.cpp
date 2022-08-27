2ce70005cef4e072df5a34f71a64227f87ec00cf
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-08-27 21:36:54 +0530

    "updated the code base"

--- cpp_projs/cp/ctf.cpp
+++ cpp_projs/cp/ctf.cpp
@@ -1,9 +1,84 @@
-import zipfile
-num = 15323 
-while (num >= 0):
-filename = "{score}" + str(num)
-with zipfile.ZipFile(filename, "r") as zip_ref:
-    zip_ref.extractall()
-    print("extracting file:", filename)
-    num -= 1
-print("done")
+// C++ code to minimize number of rotations
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// Function to check if two arrays are equal
+bool check(int A[], int B[], int N)
+{
+    bool flag = true;
+    for (int i = 0; i < N; i++) {
+        if (A[i] != B[i]) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
+ 
+// Function to left rotate an array
+void Leftrotate(int A[], int N)
+{
+    int temp = A[0];
+    for (int i = 0; i < N - 1; i++) {
+        A[i] = A[i + 1];
+    }
+    A[N - 1] = temp;
+}
+ 
+// Function to right rotate an array
+void Rightrotate(int A[], int N)
+{
+    int temp = A[N - 1];
+    for (int i = N - 1; i > 0; i--) {
+        A[i] = A[i - 1];
+    }
+    A[0] = temp;
+}
+ 
+// Function to minimize number of rotations
+int minRotations(int A[], int B[], int N)
+{
+    int C[N];
+    for (int i = 0; i < N; i++)
+        C[i] = A[i];
+    int a = 0, b = 0;
+ 
+    // Right rotate the array
+    // till it is equal to B
+    while (check(A, B, N) == false) {
+        Rightrotate(A, N);
+        a++;
+    }
+ 
+    // Left rotate the array
+    // till it is equal to B
+    while (check(C, B, N) == false) {
+        Leftrotate(C, N);
+        b++;
+    }
+ 
+    int ans = min(a, b);
+    return ans;
+}
+ 
+// Driver code
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n], b[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        for(int i=0;i<n;i++)
+            cin>>b[i];
+        int ans = minRotations(a,b,n);
+        cout << ans;
+    }
+
+ 
+    return 0;
+}

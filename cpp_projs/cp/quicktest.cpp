dd8c911d309b35cff4c99677cdb7d95b937b55ee
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-09-10 23:04:39 +0530

    "updated the code base"

--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -24,27 +24,28 @@
 //     return 0;
 // }
 
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main()
-// {
-//     clock_t start, end;
-//     start = clock();
-//     // this program is to demonstrate how to use hashing in code to reduce time complexity of the program
-//     // Question - given a number between 1 and 20, and variable number of queries, print the factorial of the number input in each query
-//     // cin>>q;
-//         // Calculates the time taken
-//     // by the algorithm to execute
 
-//     end = clock();
-//     cout << "Time: "<<fixed<<double(end-start)/double(CLOCKS_PER_SEC)<<setprecision(5)<<" s"<<endl;
-//     return 0;
-// }
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 int main()
 {
-    
+    clock_t start, end;
+    start = clock();
+
+    // start code from here
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+    }
+
+    // end code here
+
+    end = clock();
+    cout << "Time: "<<fixed<<double(end-start)/double(CLOCKS_PER_SEC)<<setprecision(5)<<" s"<<endl;
     return 0;
 }

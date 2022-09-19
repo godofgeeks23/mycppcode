23a79a3e39b4402d740dd2ab6fbba59c23684ae4
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-09-19 23:13:40 +0530

    "updated the code base"

--- cpp_projs/cp/learnCPPbasics.cpp
+++ cpp_projs/cp/learnCPPbasics.cpp
@@ -5,11 +5,5 @@ using namespace std;
 
 int main()
 {
-    int arr1[6] = {1,2,3,4};
-    // print all elements of array
-    for(int i=0; i<6; i++)
-    {
-        cout<<arr1[i]<<" ";
-    }
-    return 0;
+    
 }

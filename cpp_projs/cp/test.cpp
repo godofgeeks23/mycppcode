f5ff6685a068daf2207d8a45e55789bc05ebb756
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-09-24 22:49:48 +0530

    "updated the code base"

--- cpp_projs/cp/test.cpp
+++ cpp_projs/cp/test.cpp
@@ -1,23 +1,19 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-int minJumps(int arr[], int n){
-        // Your code here
-        int i = 0;
-        int jumps = 0;
-        while(i!=(n-1) && i<n)
-        {
-            i += arr[i];
-            // cout<<"i is on "<<arr[i]<<endl;
-            jumps++;
-        }
-        if(i>=n-1)
-            return jumps;
-        return -1;
-    }
 int main()
 {
-    int arr[6] = {1, 4, 3, 2, 6, 7};
-    cout<<minJumps(arr, 6);
+    int n, m;
+    cin>>n;
+
+    unordered_map<string, int> obs_time;
+
+    for(int i=0;i<n;i++)
+    {
+        string s;
+        cin>>s;
+        cin>>obs_time[s];
+    }
+
     return 0;
 }

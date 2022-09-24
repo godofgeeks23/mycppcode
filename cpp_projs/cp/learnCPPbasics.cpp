c6d36e484858f133db200d69929b98367250f684
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-09-24 12:23:14 +0530

    "updated the code base"

--- cpp_projs/cp/learnCPPbasics.cpp
+++ cpp_projs/cp/learnCPPbasics.cpp
@@ -2,8 +2,40 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+int max_of_four(int a,int b,int c,int d){
+if(a>b){
+    if(a>c){
+        if(a>d){
+            return a;
+        }
+    }
+}
+if(b>a){
+    if(b>c){
+        if(b>d){
+            return b;
+        }
+    }
+}
+if(c>a){
+    if(c>b){
+        if(c>d){
+            return c;
+        }
+    }
+}
+if(d>a){
+    if(d>b){
+        if(d>c){
+            return d;
+        }
+    }
+    
+}
 
+return 0;
+}
 int main()
 {
-    
+    cout<<max_of_four(1,2,3,4);
 }

41e67509b8bd510673c28fde72a040602d41bf31
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2022-10-08 22:53:08 +0530

    "updated the code base"

--- cpp_projs/cp/quicktest.cpp
+++ cpp_projs/cp/quicktest.cpp
@@ -65,55 +65,43 @@
 //     end = clock();
 //     cout << "Time: "<<fixed<<double(end-start)/double(CLOCKS_PER_SEC)<<setprecision(5)<<" s"<<endl;
 //     return 0;
-// }
+// }25 126 6 208
 
 
-//{ Driver Code Starts
+
 #include<bits/stdc++.h> 
 using namespace std; 
 
-// } Driver Code Ends
-class Solution{   
-public:
-    long maximumSumSubarray(int K, vector<int> &Arr , int N){
-        // code here 
-        int start = 0;
-        int end = K-1;
-        long sum_max = 0;
-        long sum = 0;
-        for(int i=start;i<=end;i++)
-            sum_max+=Arr[i];
-        while(end<N)
-        {
-            sum = 0;
-            start++;
-            end++;
-            for(int i=start;i<=end;i++)
-                sum+=Arr[i];
-            if(sum>sum_max)
-                sum_max = sum;
-        }
-        return sum_max;
-    }
-};
+int countpairs(int arr[], int n, int p)
+{
+    int count = 0;
+    for(int i=0;i<n-1;i++)
+        for(int j=i+1;j<n;j++)
+        if(__gcd(arr[i], arr[j])%p==0)
+            count++;
+    return count;
+}
 
-//{ Driver Code Starts.
 int main() 
 { 
     int t;
     cin>>t;
     while(t--)
     {
-        int N,K;
-        cin >> N >> K;; 
-        vector<int>Arr;
-        for(int i=0;i<N;++i){
-            int x;
-            cin>>x;
-            Arr.push_back(x);
-        }
-        Solution ob;
-        cout << ob.maximumSumSubarray(K,Arr,N) << endl;
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        int q;
+        cin>>q;
+        int queries[q];
+        for(int i=0;i<q;i++)
+            cin>>queries[i];
+
+        for(int i=0;i<q;i++)
+            cout<<countpairs(a, n, queries[i])<<" ";
+        cout<<endl;
     }
     return 0; 
 } 
